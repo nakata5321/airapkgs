8685cb86cf1601c5d47460d4d64add69abb15dfe
strdn <eilyichev@aira.life>
2018-09-25 02:28:58 +0300

    nixos/parity: add NixOS tests for parity service

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4639,6 +4639,11 @@
     github = "stites";
     name = "Sam Stites";
   };
+  strdn = {
+    email = "ilichev.evgenii@gmail.com";
+    github = "strdn";
+    name = "Evgeny Ilyichev";
+  };
   stumoss = {
     email = "samoss@gmail.com";
     github = "stumoss";
