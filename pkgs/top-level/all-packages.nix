11a4abd81f7b8f21b498aef627308bd351887d13
Alexander Krupenkin <mail@akru.me>
2018-12-25 03:28:58 +0300

    ros_environment: init at 1.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23862,6 +23862,8 @@ in
 
   roslib = callPackage ../development/ros-modules/roslib { };
 
+  ros_environment = callPackage ../development/ros-modules/ros_environment { };
+
   rospack = callPackage ../development/ros-modules/rospack { };
 
   rosunit = callPackage ../development/ros-modules/rosunit { };
