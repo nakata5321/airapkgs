7018386b851df48fd73d629334cb0601ce0fa283
Alexander Krupenkin <mail@akru.me>
2019-04-04 18:17:16 +0300

    polkadot: 0.4.0 -> 0.4.1

--- pkgs/applications/altcoins/polkadot/default.nix
+++ pkgs/applications/altcoins/polkadot/default.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   name = "polkadot-${version}";
-  version = "0.4.0";
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "polkadot";
     rev = "v${version}";
-    sha256 = "1sgj95il2gnxn1hgmsdia4hfb7pk8djgdqnfzl4y9asq693ng5j7";
+    sha256 = "1nar29h6kpvavv3sd7wh3s6zpxcf8pfz1nl56fkwnhbxqwyp0rql";
   }; 
 
-  cargoSha256 = "073xkapmg6dsvs1v2hcp2k8svzrf45dkiqxkrlpkzarbkqf79i1a";
+  cargoSha256 = "17sig942g5xxs5444ckrg6rw8f5h9dsgbmckhj43xhzb09yk6wjh";
 
   buildInputs = [ pkgconfig openssl openssl.dev clang ];
   LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
