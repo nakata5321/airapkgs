e805c9f9621a31aeda59ae53a1aabdc4d42aa97f
Alexander Krupenkin <mail@akru.me>
2019-05-21 16:26:56 +0300

    substrate-node: 0.10 -> 1.0

--- pkgs/applications/altcoins/substrate/default.nix
+++ pkgs/applications/altcoins/substrate/default.nix
@@ -10,16 +10,16 @@
 rustPlatform.buildRustPackage rec {
   name = "${pname}-${version}";
   pname = "substrate-node";
-  version = "0.10.0";
+  version = "1.0";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "substrate";
-    rev = "72409f9fd1dcf25b094dc09a31a9f831dd56411a";
-    sha256 = "0b40c2g151mm0vgh8sbz9kqxr541256x2mhdv2mskhlkkcggx5f7";
+    rev = "v${version}";
+    sha256 = "1ssvsl4qy9mv2x17vm54nsan4s75zj38mwyy8fv2m851hss98jc6";
   }; 
 
-  cargoSha256 = "1amq6zkb1aq2qjiq5xxl3zwihh0zm098nfi814fmg3h8qan5x52v";
+  cargoSha256 = "1m1cf2smw3bs34vlnxzh564cvk3iz00ay4gn9nhb0d6xxyjbcbn7";
 
   buildInputs = [ pkgconfig openssl openssl.dev clang ];
   LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
