5fce1611e76f1f2766c46930a30e04d019f1510e
strdn <eilyichev@aira.life>
2019-08-31 21:33:09 +0300

    Add variant python module

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2432,6 +2432,8 @@ in {
 
   ipfsapi = callPackage ../development/python-modules/ipfsapi { };
 
+  varint = callPackage ../development/python-modules/varint { };
+
   multiaddr = callPackage ../development/python-modules/multiaddr { };
 
   isbnlib = callPackage ../development/python-modules/isbnlib { };
