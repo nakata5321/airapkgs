d68ca3e6343dc94708564834bdfb1a529782b923
Vadim Manaenko <vadim.razorq@gmail.com>
2019-09-24 18:06:09 +0300

    Add services
    * Drone Flight Report: init at 0.4.0
    * Drone Passport:  init at 0.2.0
    * Issuing Service: init at 0.2.0

--- pkgs/applications/science/robotics/aira/issuing_service_agent/default.nix
+++ pkgs/applications/science/robotics/aira/issuing_service_agent/default.nix
@@ -8,20 +8,20 @@
 mkRosPackage rec {
   name = "${pname}-${version}";
   pname = "issuing-service-agent";
-  version = "master";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "DAO-IPCI";
-    repo = "issuing-service-package";
-    rev = "e38edc1dcb6e681f6ba8395db909d896248f9a21";
-    sha256 = "sha256:1z1xbdwbv51maqqqn0yhs72ry7rpvrvlavd5q7kjpn6x95y0knbl";
+    repo = "issuing_agent";
+    rev = "628537d4d621b399031d25e35f4ad27ab04073b9";
+    sha256 = "sha256:0mz87i6703kmcmjvdx1wqwpy2zr896y6hqpz5p7kgqrk2pdwpnab";
   };
 
   propagatedBuildInputs = [ robonomics_comm ];
 
   meta = with stdenv.lib; {
     description = "The agent is responsible for checking the data and issuing the green certificates";
-    homepage = http://github.com/DAO-IPCI/issuing-service-package;
+    homepage = http://github.com/DAO-IPCI/issuing_agent;
     license = licenses.bsd3;
     maintainers = with maintainers; [ vourhey ];
   };
