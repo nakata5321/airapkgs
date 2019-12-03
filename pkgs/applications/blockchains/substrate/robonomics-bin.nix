736ee91fe0e77accfbf05e52d3cacac9ee0fc273
Pavel Sheremetev <spd@aira.life>
2019-12-04 00:19:38 +0400

    substrate-node-robonomics-bin: 0.11 -> 0.13

--- pkgs/applications/blockchains/substrate/robonomics-bin.nix
+++ pkgs/applications/blockchains/substrate/robonomics-bin.nix
@@ -9,13 +9,13 @@ let
     else if stdenv.hostPlatform.system == "aarch64-linux" then "linux-aarch64"
     else throw "Encryptr for ${stdenv.hostPlatform.system} not supported!";
 
-  sha256 = if stdenv.hostPlatform.system == "x86_64-linux" then "11h97404vknicy98qha0ccwnjgzl7i14zg0yx1rr1dclq1735ajd"
-    else if stdenv.hostPlatform.system == "aarch64-linux" then "1r83v2sm1f7yzpcd6c3sb7sp00i70ffwqm6v4i0sfmx8m3rasgfz"
+  sha256 = if stdenv.hostPlatform.system == "x86_64-linux" then "1z1ykm1nzcijgjjcgwajyvywqr1xkbqpsxxzlgab2myyg9msrpaj"
+    else if stdenv.hostPlatform.system == "aarch64-linux" then "0p2568fprrbfaxg0ci3hlxpd2kl0pz9d0h4yjrl0v524vixhxry4"
     else throw "Encryptr for ${stdenv.hostPlatform.system} not supported!";
 
 in stdenv.mkDerivation rec {
    name = "substrate-node-robonomics-bin-${version}";
-   version = "0.11";
+   version = "0.13";
    repoUrl = "https://github.com/airalab/substrate-node-robonomics";
    src = fetchurl {
       url = "${repoUrl}/releases/download/v${version}/robonomics-${version}-${arch}.tgz";
