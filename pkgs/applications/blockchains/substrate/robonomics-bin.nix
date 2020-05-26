700cc47c9a84de3fc08c3b5d8e53cac2ba2996eb
Vadim Manaenko <vadim.razorq@gmail.com>
2020-05-26 14:30:41 +0300

    Update robonomics-bin.nix
    robonomics: 0.13 -> 0.18

--- pkgs/applications/blockchains/substrate/robonomics-bin.nix
+++ pkgs/applications/blockchains/substrate/robonomics-bin.nix
@@ -2,23 +2,25 @@
 , fetchurl
 , libgcc
 , openssl
+, zlib
+, systemd
 }:
 
 let
-  arch = if stdenv.hostPlatform.system == "x86_64-linux" then "linux-x86_64"
+  arch = if stdenv.hostPlatform.system == "x86_64-linux" then "x86_64"
     else if stdenv.hostPlatform.system == "aarch64-linux" then "linux-aarch64"
     else throw "Encryptr for ${stdenv.hostPlatform.system} not supported!";
 
-  sha256 = if stdenv.hostPlatform.system == "x86_64-linux" then "1z1ykm1nzcijgjjcgwajyvywqr1xkbqpsxxzlgab2myyg9msrpaj"
+  sha256 = if stdenv.hostPlatform.system == "x86_64-linux" then "1za3kf0lcw74z1sl6r5sixf7wyrw1ijchg30rc9fr41dk0pcpsrm"
     else if stdenv.hostPlatform.system == "aarch64-linux" then "0p2568fprrbfaxg0ci3hlxpd2kl0pz9d0h4yjrl0v524vixhxry4"
     else throw "Encryptr for ${stdenv.hostPlatform.system} not supported!";
 
 in stdenv.mkDerivation rec {
    name = "substrate-node-robonomics-bin-${version}";
-   version = "0.13";
-   repoUrl = "https://github.com/airalab/substrate-node-robonomics";
+   version = "0.18.0";
+   repoUrl = "https://github.com/airalab/robonomics";
    src = fetchurl {
-      url = "${repoUrl}/releases/download/v${version}/robonomics-${version}-${arch}.tar.xz";
+      url = "${repoUrl}/releases/download/v${version}/robonomics-ubuntu-${version}-${arch}.tar.xz";
       inherit sha256;
    };
   dontBuild = true;
@@ -27,13 +29,15 @@ in stdenv.mkDerivation rec {
     [ libgcc
       openssl
       stdenv.cc.cc.lib # libstdc++.so.6
+      zlib
+      systemd.lib
     ];
 
   phases = "installPhase fixupPhase";
 
   installPhase = ''
    mkdir -p $out/bin
-   tar -xzvf ${src} --directory $out/bin
+   tar -xf ${src} --directory $out/bin
    chmod +x $out/bin/robonomics
    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
    --set-rpath "$libPath" $out/bin/robonomics
