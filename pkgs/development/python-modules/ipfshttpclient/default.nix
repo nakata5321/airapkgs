0432390ac9844c88646ba6f6955f6baec97789b9
Vadim Manaenko <vadim.razorq@gmail.com>
2020-06-17 13:55:28 +0300

    ipfshttpclient: 0.4.12 -> 0.4.13.2

--- pkgs/development/python-modules/ipfshttpclient/default.nix
+++ pkgs/development/python-modules/ipfshttpclient/default.nix
@@ -4,21 +4,25 @@
 , lib
 , multiaddr
 , requests
+, fetchFromGitHub
 }:
 
-buildPythonPackage rec {
-  name = "${pname}-${version}";
+buildPythonPackage {
   pname = "ipfshttpclient";
-  version = "0.4.12";
+  version = "0.4.13.2";
 
   disabled = isPy27;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "1dwvx0zy53vi4qg9vb5s02krrc5hhm3szd98vbwvyi7y0l89l68a";
+  src = fetchFromGitHub {
+    owner = "ipfs-shipyard";
+    repo = "py-ipfs-http-client";
+    rev = "5e3edf5f8e9e117738c77b5773c78d7b48942bc2";
+    sha256 = "1i3vw1fxck6ail99lm47d8q3g259s0fqsjmjagyfxb5zjvkh30y1";
   };
 
   propagatedBuildInputs = [ multiaddr requests ];
+
+  patches = [ ./ipfshttpclient.patch ];
 
   meta = {
     description = "A python client library for the IPFS API.";
