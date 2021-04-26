e7a22b3b658ec03b4425a0c880e01eaa6710bb91
Pavel Sheremetev <spd@aira.life>
2021-04-26 07:36:22 +0400

    FIX maintainer name

--- pkgs/development/python-modules/py-bip39-bindings/default.nix
+++ pkgs/development/python-modules/py-bip39-bindings/default.nix
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     description = "Python bindings for the tiny-bip39 library";
     homepage = "https://github.com/polkascan/py-ed25519-bindings";
     license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ Vourhey ];
+    maintainers = with lib.maintainers; [ vourhey ];
   };
 }
 
