c5cad51d02018c8bc307d0fb534f9fd2c4ed9a33
PavelSheremetev <spd@aira.life>
2022-01-26 01:22:41 +0400

    update infura ENDPOINTS

--- nixos/modules/profiles/aira-foundation-nightly.nix
+++ nixos/modules/profiles/aira-foundation-nightly.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 
 let
-  web3_http_provider = "https://mainnet.infura.io/v3/cd7368514cbd4135b06e2c5581a4fff7";
+  web3_http_provider = "https://mainnet.infura.io/v3/61518717e71a4f638fefd247f7a6617b";
   web3_ws_provider = "wss://mainnet.infura.io/ws";
   lighthouse = "airalab.lighthouse.5.robonomics.eth";
   factory = "factory.5.robonomics.eth";
